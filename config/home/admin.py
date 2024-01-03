a45219ba3f603bf8bb743b6162bda9dbbf1d9314
sinanazem <sinanaazem@gmail.com>
2024-01-03 22:10:24 +0330

    update admin

--- config/home/admin.py
+++ config/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 from .models import PostModel
-admin.site.register(PostModel)
+
+@admin.register(PostModel)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', 'body')
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
+
+
