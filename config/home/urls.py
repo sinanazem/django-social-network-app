2f10cd7909905aaf1ae46cea6d42af86682d9625
sinanazem <sinanaazem@gmail.com>
2024-04-29 17:38:12 +0330

    update

--- config/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomeView, CommunityView
-
-app_name = 'home'
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('community/', CommunityView.as_view(), name='community'),
-]
